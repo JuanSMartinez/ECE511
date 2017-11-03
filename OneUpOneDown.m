5dbd2ae6fa02b50b4a893f8025da85bc5b7b1bc3
JuanSMartinez <js.martinez777@uniandes.edu.co>
2017-11-03 12:09:34 -0400

    constructor of 1u1d object

--- OneUpOneDown.m
+++ OneUpOneDown.m
@@ -2,7 +2,7 @@ classdef OneUpOneDown
     %OneUpOneDown  One up one down adaptive method
     %   Adaptive method for the snake project
     
-    properties
+    properties (SetAccess=private)
         inital_step;
         direction;
         step;
@@ -11,6 +11,25 @@ classdef OneUpOneDown
     end
     
     methods
+        function obj=OneUpOneDown(initial_step, direction, step, final_reversals)
+            if(direction ~= 0 && direction ~= 1)
+                %0 means decreasing, 1 means increasing
+                error('Direction identifier is not valid');
+            elseif ( initial_step <= 0)
+                error('Initial step size must be positive');
+            elseif ( step <= 0)
+                error('Step size must be positive');
+            elseif ( final_reversals <= 0)
+                error('Final_reversals be positive'); 
+            else
+                obj.inital_step = initial_step;
+                obj.direction = direction;
+                obj.step = step;
+                obj.reversals = 0;
+                obj.final_reversals = final_reversals;
+            end
+          
+        end
     end
     
 end
