dcadaf81e67de3b02df8ab4dd9af2c135fa67984
JuanSMartinez <js.martinez777@uniandes.edu.co>
2017-11-03 11:51:37 -0400

    added one up one down class

---
+++ OneUpOneDown.m
@@ -0,0 +1,17 @@
+classdef OneUpOneDown
+    %OneUpOneDown  One up one down adaptive method
+    %   Adaptive method for the snake project
+    
+    properties
+        inital_step;
+        direction;
+        step;
+        reversals;
+        final_reversals;
+    end
+    
+    methods
+    end
+    
+end
+
