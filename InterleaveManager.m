2c43094fbada07fbbcb96f853d74462ec105ab38
JuanSMartinez <js.martinez777@uniandes.edu.co>
2017-11-03 16:22:09 -0400

    interleaver manager class

---
+++ InterleaveManager.m
@@ -0,0 +1,45 @@
+classdef InterleaveManager<handle
+    %INTERLEAVEMANAGER manages the interleaved methods
+    %   Performs the experiment with two interleaved 1U1D and an SOA as
+    %   parameter for each one
+    
+    properties(SetAccess=private)
+        adaptive_methods;
+    end
+    
+    methods
+        function obj=InterleaveManager(initialSOA, modulation, initial_step, step, final_reversals_per_method)
+            method_1=OneUpOneDown(initial_step, -1, step, final_reversals_per_method, initialSOA);
+            method_2=OneUpOneDown(initial_step, 1, step, final_reversals_per_method, initialSOA);
+            adaptive_methods = cell(1,2);
+            adaptive_methods{1} = method_1;
+            adaptive_methods{2} = method_2;
+        end
+        
+        function performed = perform_trial(obj,user_answer)
+           % Get a random method to perform
+           if(~obj.experiment_finished())
+               num = randi(2,1,1);
+               method = obj.adaptive_methods{num};
+               while(method.finished() && ~obj.experiment_finished())
+                   num = randi(2,1,1);
+                   method = obj.adaptive_methods{num};
+               end
+               method.perform_trial(user_answer);
+               performed = 1;
+           else
+               performed = 0;
+           end
+        end
+        
+        function stop = experiment_finished(obj)
+           stop = obj.adaptive_methods{1}.finished() && obj.adaptive_methods{2}.finished();
+        end
+        
+        function estimate = calculate_result(obj)
+           estimate = (obj.adaptive_methods{1}.calculate_final_estimate()+obj.adaptive_methods{2}.calculate_final_estimate())/2;
+        end
+    end
+    
+end
+
