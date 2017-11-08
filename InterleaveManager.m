b1e6be5f2e970ec2d706a8704237d9e27eb1dfa7
JuanSMartinez <js.martinez777@uniandes.edu.co>
2017-11-08 15:10:45 -0500

    finished upper soa experiment without snake function

--- InterleaveManager.m
+++ InterleaveManager.m
@@ -14,12 +14,12 @@ classdef InterleaveManager<handle
         function obj=InterleaveManager(sig_intensities, sig_duration, initialSOA_1, initialSOA_2, modulation, initial_step, step, final_reversals_per_method)
             method_1=OneUpOneDown(initial_step, 0, step, final_reversals_per_method, initialSOA_1);
             method_2=OneUpOneDown(initial_step, 1, step, final_reversals_per_method, initialSOA_2);
-            adaptive_methods = cell(1,2);
-            adaptive_methods{1} = method_1;
-            adaptive_methods{2} = method_2;
-            signal_intensities = sig_intensities;
-            signal_duration = sig_duration;
-            signal_modulation = modulation;
+            obj.adaptive_methods = cell(1,2);
+            obj.adaptive_methods{1} = method_1;
+            obj.adaptive_methods{2} = method_2;
+            obj.signal_intensities = sig_intensities;
+            obj.signal_duration = sig_duration;
+            obj.signal_modulation = modulation;
         end
         
         function performed = perform_trial(obj,user_answer)
@@ -38,10 +38,11 @@ classdef InterleaveManager<handle
                % snake = adjust_snake(snake_matrix, obj.signal_intensities)
                % playrec('play', snake)
                % playrec('block')
+               
                %dummy snake
                snake = eye(21168,24);
                snake_final = adjust_snake(snake, obj.signal_intensities);
-               playrec('play', snake_final)
+               playrec('play', snake_final, 1:1:24);
                playrec('block')
                performed = 1;
            else
@@ -55,6 +56,10 @@ classdef InterleaveManager<handle
         
         function estimate = calculate_result(obj)
            estimate = (obj.adaptive_methods{1}.calculate_final_estimate()+obj.adaptive_methods{2}.calculate_final_estimate())/2;
+        end
+        
+        function method_progress = get_methods_progress(obj)
+           method_progress = [obj.adaptive_methods{1}.parameter, obj.adaptive_methods{2}.parameter];
         end
     end
     
